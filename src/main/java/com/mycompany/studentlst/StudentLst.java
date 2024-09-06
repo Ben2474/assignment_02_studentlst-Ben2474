675b772d0ec59c9be4b3b405bf8d889302178c2c
Ben2474 <bennettthomas24@gmail.com>
2024-09-06 18:01:37 -0400

    Completed the implementation of the printOutput method in the StudentList class

--- src/main/java/com/mycompany/studentlst/StudentLst.java
+++ src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -1,34 +1,29 @@
 package com.mycompany.studentlst;
-
 import java.util.Scanner;
-
 /**
  * @author MoaathAlrajab
  */
 public class StudentLst {
+public static void main(String[] args) {
+Student[] students = new Student[2];
+Scanner inScanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Student[] students= new Student[2];
-        Scanner inScanner = new Scanner(System.in);
-        
-        for(int i=0; i< students.length;i++){
-            System.out.print("Enter student's name: ");
-            String name = inScanner.nextLine();
-            Student student = new Student(name);
-            
-            students[i]= student;
-            
-        }
-        System.out.println(printOutput(students));
-    }
-    
-    public static String printOutput(Student[] students){
-        String str="";
-        
-        //To Do 03: complete the implementation of this method  
-        
-        return str;
-    }
-    
-    
+for (int i = 0; i < students.length; i++) {
+System.out.print("Enter student's name: ");
+String name = inScanner.nextLine();
+Student student = new Student(name);
+students[i] = student;
+}
+System.out.println(printOutput(students));
+}
+
+public static String printOutput(Student[] students){
+String str="";
+//To Do 03: complete the implementation of this method
+str += "Student List: \n";
+for (int i = 0; i<students.length;i++) {
+str += (i+1) + ". " + students[i].getName() + "\n";
+}
+return str;
+}
 }
